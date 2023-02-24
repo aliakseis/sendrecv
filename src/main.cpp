7f103f9a6e5627823b88c072ad1dd8730377b611
aliakseis <aliaksei.sanko@gmail.com>
2023-02-24 16:33:38 +0300

    optional ntfy.sh support for signaling added

--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,32 @@
 #define GST_USE_UNSTABLE_API
 #include <gst/webrtc/webrtc.h>
 
+#define USE_NTFY 0
+
 /* For signalling */
+#if USE_NTFY
+
+#include "http.h"
+
+#else
+
 #include <libsoup/soup.h>
+
+#endif
+
 #include <json-glib/json-glib.h>
+
+#include <crossguid/guid.hpp>
+
 
 #include <string.h>
 
 #include <string>
+#include <string_view>
+
+#include <future>
+#include <atomic>
+
 
 enum AppState
 {
@@ -50,20 +69,44 @@ static GstElement *pipe1, *webrtc1 = NULL;
 static GObject *send_channel, *receive_channel;
 
 static AppState app_state = APP_STATE_UNKNOWN;
+
+
+#if USE_NTFY
+
+static gchar *session_id = nullptr;
+
+#else
+
 static gchar *peer_id = NULL;
 static gchar *our_id = NULL;
+
 static const gchar *server_url = "wss://webrtc.nirbheek.in:8443";
 static gboolean disable_ssl = FALSE;
+
+
+#endif
+
 static gboolean remote_is_offerer = FALSE;
 
 static GOptionEntry entries[] = {
+
+#if USE_NTFY
+
+  {"session-id", 0, 0, G_OPTION_ARG_STRING, &session_id,
+      "String ID of the session that peer can connect to", "ID"},
+
+#else
+
   {"peer-id", 0, 0, G_OPTION_ARG_STRING, &peer_id,
-      "String ID of the peer to connect to", "ID"},
+        "String ID of the peer to connect to", "ID"},
   {"our-id", 0, 0, G_OPTION_ARG_STRING, &our_id,
-      "String ID of the session that peer can connect to us", "ID"},
+        "String ID of the session that peer can connect to us", "ID"},
   {"server", 0, 0, G_OPTION_ARG_STRING, &server_url,
-      "Signalling server to connect to", "URL"},
+        "Signalling server to connect to", "URL"},
   {"disable-ssl", 0, 0, G_OPTION_ARG_NONE, &disable_ssl, "Disable ssl", NULL},
+
+#endif
+
   {"remote-offerer", 0, 0, G_OPTION_ARG_NONE, &remote_is_offerer,
       "Request that the peer generate the offer and we'll answer", NULL},
   {NULL},
@@ -73,7 +116,7 @@ static GOptionEntry entries[] = {
 static gboolean
 cleanup_and_quit_loop(const gchar * msg, enum AppState state);
 
-static void on_server_message(gchar *text);
+static void on_server_message(const gchar *text);
 
 static gboolean
 start_pipeline(gboolean create_offer);
@@ -81,7 +124,182 @@ start_pipeline(gboolean create_offer);
 
 ////////////////////////////////////////////////////////////////////
 
+#if USE_NTFY
+
+const xg::Guid this_guid = xg::newGuid();
+
+xg::Guid their_giud;
+
+std::thread signaling_runner;
+
+static std::atomic_bool requestInterrupted = false;
+
+const char send_message_url[] = "https://ntfy.sh/mediaThorSendRecv_%s";
+const char recv_message_url[] = "https://ntfy.sh/mediaThorSendRecv_%s/sse";
+
+
+static bool we_create_offer()
+{
+    return this_guid.bytes() < their_giud.bytes();
+}
+
+
+static void signaling_connection_send_text(gchar *text)
+{
+    const auto message = this_guid.str() + '\n' + text;
+
+    char buffer[1024];
+    sprintf(buffer, send_message_url, session_id);
+    http(HTTP_POST, buffer, nullptr, message.c_str(), message.length());
+}
+
+static const char* verify_sse_response(CURL* curl) {
+#define EXPECTED_CONTENT_TYPE "text/event-stream"
+
+    static const char expected_content_type[] = EXPECTED_CONTENT_TYPE;
+
+    const char* content_type;
+    curl_easy_getinfo(curl, CURLINFO_CONTENT_TYPE, &content_type);
+    if (!content_type) content_type = "";
+
+    if (!strncmp(content_type, expected_content_type, strlen(expected_content_type)))
+        return 0;
+
+    return "Invalid content_type, should be '" EXPECTED_CONTENT_TYPE "'.";
+}
+
+static bool connect_to_signaling_server_async()
+{
+    std::promise<bool> startedPromise;
+
+    auto startedResult = startedPromise.get_future();
+
+    auto threadLam = [](
+        std::promise<bool> startedPromise
+        ) {
+            const char* headers[] = {
+                "Accept: text/event-stream",
+                NULL
+            };
+
+            auto on_data = [&startedPromise](char *ptr, size_t size, size_t nmemb)->size_t {
+                try {
+                    const auto ptrEnd = ptr + size * nmemb;
+
+                    const char watch[] = "data:";
+
+                    auto pData = std::search(ptr, ptrEnd, std::begin(watch), std::prev(std::end(watch)));
+                    if (pData != ptrEnd) do {
+                        pData += sizeof(watch) / sizeof(watch[0]) - 1;
+
+                        JsonParser *parser = json_parser_new();
+                        if (!json_parser_load_from_data(parser, pData, ptrEnd - pData, NULL)) {
+                            //gst_printerr("Unknown message '%s', ignoring\n", text);
+                            g_object_unref(parser);
+                            break; //goto out;
+                        }
+
+                        auto root = json_parser_get_root(parser);
+                        if (!JSON_NODE_HOLDS_OBJECT(root)) {
+                            //gst_printerr("Unknown json message '%s', ignoring\n", text);
+                            g_object_unref(parser);
+                            break; //goto out;
+                        }
+
+                        auto child = json_node_get_object(root);
+
+                        if (!json_object_has_member(child, "event")) {
+                            g_object_unref(parser);
+                            break; //goto out;
+                        }
+
+                        auto sdptype = json_object_get_string_member(child, "event");
+                        if (g_str_equal(sdptype, "open")) {
+                            startedPromise.set_value(true);
+                        }
+                        else if (g_str_equal(sdptype, "message")) {
+                            auto text = json_object_get_string_member(child, "message");
+
+                            if (auto pos = strchr(text, '\n'); pos != nullptr && pos != text)
+                            {
+                                std::string_view sender_guid(text, pos - text);
+                                if (this_guid.str() != sender_guid)
+                                {
+                                    if (!their_giud.isValid())
+                                        their_giud = std::string(sender_guid);
+
+                                    const auto message = pos + 1;
+                                    const bool is_syn = g_strcmp0(message, "SYN") == 0;
+                                    if (is_syn)
+                                        signaling_connection_send_text("ACK");
+
+                                    if (is_syn || g_strcmp0(message, "ACK") == 0) {
+                                        if (app_state < PEER_CONNECTED) {
+                                            app_state = PEER_CONNECTED;
+                                            /* Start negotiation (exchange SDP and ICE candidates) */
+                                            if (we_create_offer() && !start_pipeline(TRUE))
+                                                cleanup_and_quit_loop("ERROR: failed to start pipeline",
+                                                    PEER_CALL_ERROR);
+                                        }
+                                    }
+                                    else
+                                        on_server_message(message);
+                                }
+                            }
+                        }
+
+                        g_object_unref(parser);
+
+                    } while (false);
+                }
+                catch (const std::exception&) {
+                    startedPromise.set_value(false);
+                    requestInterrupted = true;
+                }
+                return size * nmemb;
+            };
+
+            auto progress_callback = [](curl_off_t dltotal,
+                curl_off_t dlnow,
+                curl_off_t ultotal,
+                curl_off_t ulnow)->size_t {
+                    return requestInterrupted;
+            };
+
+            char buffer[1024];
+            sprintf(buffer, recv_message_url, session_id);
+            http(HTTP_GET, buffer, headers, 0, 0, on_data, verify_sse_response, progress_callback);
+    };
+
+    // https://stackoverflow.com/a/23454840/10472202
+    signaling_runner = std::thread(threadLam, std::move(startedPromise));
+
+    if (!startedResult.get())
+        return false;
+
+    signaling_connection_send_text("SYN");
+
+    return true;
+}
+
+static void close_signaling_connection()
+{
+    if (signaling_runner.joinable())
+    {
+        requestInterrupted = true;
+        signaling_runner.join();
+    }
+}
+
+
+#else
+
 static SoupWebsocketConnection *ws_conn = NULL;
+
+static bool we_create_offer()
+{
+    return !our_id;
+}
 
 static void close_signaling_connection()
 {
@@ -287,6 +505,10 @@ connect_to_signaling_server_async(void)
         (GAsyncReadyCallback)on_server_connected, message);
     app_state = SERVER_CONNECTING;
 }
+
+#endif
+
+
 
 ////////////////////////////////////////////////////////////////////
 
@@ -799,7 +1021,7 @@ on_offer_received (GstSDPMessage * sdp)
 }
 
 /* One mega message handler for our asynchronous calling mechanism */
-static void on_server_message(gchar *text) {
+static void on_server_message(const gchar *text) {
   if (g_strcmp0 (text, "OFFER_REQUEST") == 0) {
     if (app_state != SERVER_REGISTERED) {
       gst_printerr ("Received OFFER_REQUEST at a strange time, ignoring\n");
@@ -850,7 +1072,7 @@ static void on_server_message(gchar *text) {
 
     /* If peer connection wasn't made yet and we are expecting peer will
      * connect to us, launch pipeline at this moment */
-    if (!webrtc1 && our_id) {
+    if (!webrtc1 && !we_create_offer()) {
       if (!start_pipeline (FALSE)) {
         cleanup_and_quit_loop ("ERROR: failed to start pipeline",
             PEER_CALL_ERROR);
@@ -977,6 +1199,15 @@ main (int argc, char *argv[])
     goto out;
   }
 
+#if USE_NTFY
+
+  if (!session_id) {
+      gst_printerr("--session-id is a required argument\n");
+      goto out;
+  }
+
+#else
+
   if (!peer_id && !our_id) {
     gst_printerr ("--peer-id or --our-id is a required argument\n");
     goto out;
@@ -986,6 +1217,8 @@ main (int argc, char *argv[])
     gst_printerr ("specify only --peer-id or --our-id\n");
     goto out;
   }
+
+#endif
 
   ret_code = 0;
 
@@ -1015,8 +1248,17 @@ main (int argc, char *argv[])
   }
 
 out:
+
+#if USE_NTFY
+
+  g_free(session_id);
+
+#else
+
   g_free (peer_id);
   g_free (our_id);
+
+#endif
 
   return ret_code;
 }
